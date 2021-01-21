4fc4641551b2640ccb510c944fb4b43e5c4a7ad9
Thebarda <tomdar87@outlook.com>
2021-01-21 11:22:12 +0100

    Add border style

--- app3/src/components/Hello.js
+++ app3/src/components/Hello.js
@@ -2,9 +2,9 @@ import * as React from 'react';
 
 function Hello() {
   return (
-    <>
-      <h3>Hello</h3>
-    </>
+    <div style={{ border: '1px solid blue' }}>
+      <h3>Hello from module 3</h3>
+    </div>
   );
 }
 
