1acb74dcaf66a585f687ef55a4a3573d81c32220
Tom Darneix <tomdar87@outlook.com>
2021-10-23 13:07:20 +0200

    Sanitize the project (2)

--- app3/src/components/Hello.js
+++ app3/src/components/Hello.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 function Hello() {
   return (
-    <div style={{ border: '1px solid blue' }}>
+    <div style={{ border: '1px solid red' }}>
       <h3>Hello from the other side</h3>
     </div>
   );
